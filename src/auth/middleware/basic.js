4644780f9a28183732490b4c84a1024de9159d40
saleh2001k <saleh.ayman2001@gmail.com>
2023-07-04 06:10:36 +0300

    test02

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const base64 = require('base-64');
-const { users } = require('../models');
+const { users } = require('../../models/index');
 
 module.exports = async (req, res, next) => {
 
